7298231b90efd1c791c8fd113f30152bf25aa0dd
yu.xiang <yu.xiang@alibaba-inc.com>
2021-01-03 14:25:37 +0800

    add_code_for_job

---
+++ jobs/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from django.urls import path
+from django.conf import settings
+
+from jobs import views
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns = [
+    # 职位列表
+    path("job_list/", views.job_list, name="job_list"),
+
+    # 管理员创建 HR 账号的 页面:
+    # path('create_hr_user/', views.create_hr_user, name='create_hr_user'),
+
+    # 职位详情
+    url(r'^job_detail/(?P<job_id>\d+)/$', views.job_detail, name='detail'),
+    # path('job_detail/<int:job_id>/', views.detail, name='job_detail'),
+
+    # path('resume/add/', views.ResumeCreateView.as_view(), name='resume-add'),
+    # path('resume/<int:pk>/', views.ResumeDetailView.as_view(), name='resume-detail'),
+
+    # path('sentry-debug/', trigger_error),
+
+    # 首页自动跳转到 职位列表
+    url(r"^$", views.job_list, name="name"),
+    # path("", views.job_list, name="name"),
+
+]
---
+++ jobs/views.py
@@ -0,0 +1,28 @@
+from django.http import Http404
+from django.shortcuts import render
+from jobs.models import Job
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+
+def job_list(request):
+    job_list = Job.objects.order_by('job_type')
+    context =  {'job_list': job_list}
+    for job in job_list:
+        job.city_name = Job.Cities[job.job_city][1]
+        job.type_name = Job.JobTypes[job.job_type][1]
+    return render(request, 'job_list.html', context)
+
+
+def job_detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.city_name = Job.Cities[job.job_city][1]
+        logger.info('job retrieved from db :%s' % job_id)
+    except Job.DoesNotExist:
+        raise Http404("Job does not exist")
+    return render(request, 'job_detail.html', {'job': job})
+
