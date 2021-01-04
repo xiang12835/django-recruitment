b39514333bbedaed10e471d45774f79cc515ce73
yu.xiang <yu.xiang@alibaba-inc.com>
2021-01-04 23:00:31 +0800

    x

--- interview/admin.py
+++ interview/admin.py
@@ -43,11 +43,18 @@ def export_model_as_csv(modeladmin, request, queryset):
 
 
 export_model_as_csv.short_description = u'导出为CSV文件'
+export_model_as_csv.allowed_permissions = ('export',)
+
 
 # 候选人管理类
 class CandidateAdmin(admin.ModelAdmin):
 
     actions = (export_model_as_csv, )
+
+    # 导出功能权限：当前用户是否有导出权限：
+    def has_export_permission(self, request):
+        opts = self.opts
+        return request.user.has_perm('%s.%s' % (opts.app_label, "export"))
 
     # 排除一些属性，此时数据库也会没有
     exclude = ('creator', 'created_date', 'modified_date')
@@ -116,7 +123,7 @@ class CandidateAdmin(admin.ModelAdmin):
                                     "second_interviewer_user", "second_remark",)}),
     )
 
-    # 一面面试官仅填写一面反馈， 二面面试官可以填写二面反馈
+    # 数据权限：一面面试官仅填写一面反馈， 二面面试官可以填写二面反馈
     def get_fieldsets(self, request, obj=None):
         group_names = self.get_group_names(request.user)
 
@@ -126,7 +133,7 @@ class CandidateAdmin(admin.ModelAdmin):
             return self.default_fieldsets_second
         return self.default_fieldsets
 
-    # 对于非管理员，非HR，获取自己是一面面试官或者二面面试官的候选人集合:s
+    # 数据集的权限：对于非管理员，非HR，获取自己是一面面试官或者二面面试官的候选人集合:s
     def get_queryset(self, request):  # show data only owned by the user
         qs = super(CandidateAdmin, self).get_queryset(request)
 
