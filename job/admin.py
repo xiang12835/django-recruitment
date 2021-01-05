2810d3e3cb68eea979df0935f4f5d52de0b7011b
yu.xiang <yu.xiang@alibaba-inc.com>
2021-01-05 14:35:08 +0800

    add_resume

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from job.models import Job
+from job.models import Job, Resume
+
 
 class JobAdmin(admin.ModelAdmin):
     exclude = ('creator','created_date','modified_date')
@@ -10,7 +11,27 @@ class JobAdmin(admin.ModelAdmin):
             obj.creator = request.user
         super().save_model(request, obj, form, change)
 
+class ResumeAdmin(admin.ModelAdmin):
+
+
+    list_display = ('username', 'applicant', 'city', 'apply_position', 'bachelor_school', 'master_school', 'major','created_date')
+
+    readonly_fields = ('applicant', 'created_date', 'modified_date',)
+
+    fieldsets = (
+        (None, {'fields': (
+            "applicant", ("username", "city", "phone"),
+            ("email", "apply_position", "born_address", "gender", ), ("picture", "attachment",),
+            ("bachelor_school", "master_school"), ("major", "degree"), ('created_date', 'modified_date'),
+            "candidate_introduction", "work_experience","project_experience",)}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.applicant = request.user
+        super().save_model(request, obj, form, change)
+
 
 
 # Register your models here.
 admin.site.register(Job, JobAdmin)
+admin.site.register(Resume, ResumeAdmin)
