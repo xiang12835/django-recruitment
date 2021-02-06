ee73c6e12f63df3ef03e1cf559a2e18446971b3b
shane <953762287@qq.com>
2021-02-06 15:36:11 +0800

    add_performance_logger_middleware

---
+++ interview/performance.py
@@ -0,0 +1,22 @@
+import time
+import logging
+
+from django.http import HttpResponse
+# import traceback
+
+# from sentry_sdk import capture_exception
+# from . import dingtalk
+
+logger = logging.getLogger(__name__)
+
+
+def performance_logger_middleware(get_response):
+    def middleware(request):
+        start_time = time.time()
+        response = get_response(request)
+        duration = time.time() - start_time
+        response["X-Page-Duration-ms"] = int(duration * 1000)
+        logger.info("%s %s %s", duration, request.path, request.GET.dict() )
+        return response
+
+    return middleware
--- settings/base.py
+++ settings/base.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# LOG_DIR = "/data/logs/recruitment/"
+LOG_DIR = "/tmp/logs/recruitment/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'interview.performance.performance_logger_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -193,12 +194,12 @@ LOGGING = {
             'filename': os.path.join(os.path.dirname(BASE_DIR), 'recruitment.admin.log'),
         },
 
-        # 'performance': {
-        #     #'level': 'INFO',
-        #     'class': 'logging.FileHandler',
-        #     'formatter': 'simple',
-        #     'filename': os.path.join(LOG_DIR, 'recruitment.performance.log'),
-        # },
+        'performance': {
+            #'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'simple',
+            'filename': os.path.join(LOG_DIR, 'recruitment.performance.log'),
+        },
     },
 
     'root': {
@@ -212,10 +213,10 @@ LOGGING = {
             "level": "DEBUG",
         },
 
-        # "interview.performance": {
-        #     "handlers": ["console", "performance"],
-        #     "level": "INFO",
-        #     "propagate": False,
-        # },
+        "interview.performance": {
+            "handlers": ["console", "performance"],
+            "level": "INFO",
+            "propagate": False,
+        },
     },
 }
