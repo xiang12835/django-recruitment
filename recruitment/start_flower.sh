c8cf7e1604e99bad0a7b11edc35baeabc21e0f38
shane <953762287@qq.com>
2021-04-05 19:20:42 +0800

    upd

--- recruitment/start_flower.sh
+++
@@ -1,2 +0,0 @@
-# https://docs.celeryproject.org/en/stable/userguide/monitoring.html
-DJANGO_SETTINGS_MODULE=settings.local celery -A recruitment flower
--- recruitment/start_worker.sh
+++
@@ -1,2 +0,0 @@
-# https://docs.celeryproject.org/en/stable/getting-started/first-steps-with-celery.html
-DJANGO_SETTINGS_MODULE=settings.local celery -A recruitment worker -l info
