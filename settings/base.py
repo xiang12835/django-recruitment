10a6f181c4085c2adeae57ba051fc9120e50724e
shane <953762287@qq.com>
2021-04-05 19:09:21 +0800

    add_beat

--- settings/base.py
+++ settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_python3_ldap',
+    'django_celery_beat',
     'rest_framework',
     'job',
     'interview'
